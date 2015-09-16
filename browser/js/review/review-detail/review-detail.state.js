a893ae8fdabf5c06d56d26951ab53de7b7bee022
Sean Johnston <sean.johnst@gmail.com>
2015-09-16 18:19:24 -0400

    delete commented resolve

--- browser/js/review/review-detail/review-detail.state.js
+++ browser/js/review/review-detail/review-detail.state.js
@@ -3,15 +3,5 @@ app.config(function($stateProvider){
   .state('reviewDetail', {
     url: '/review/:reviewId',
     templateUrl: '/js/review/review-detail/review-detail.html'
-    // resolve:{
-    //   reviewFinder: function(ReviewFactory, $stateParams){
-    //     return ReviewFactory.getReview($stateParams.reviewId);
-    //   }
-    // },
-    // controller: function ($scope, reviewFinder) {
-    //   $scope.review = reviewFinder;
-    //   $scope.user = reviewFinder.user;
-    //   $scope.product = reviewFinder.product;
-    // }
   });
 });
