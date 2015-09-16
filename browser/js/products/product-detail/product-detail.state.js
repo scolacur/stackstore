04b8320f1e21d662b9aa021663ce1cfbd899cd0b
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-09-16 12:40:11 -0400

    product fix

--- browser/js/products/product-detail/product-detail.state.js
+++ browser/js/products/product-detail/product-detail.state.js
@@ -2,20 +2,7 @@ app.config(function ($stateProvider) {
 
     $stateProvider.state('productDetail', {
         url: '/products/:id',
-        templateUrl: '/js/products/product-detail/product-detail.html',
-        controller: function ($scope, productFinder) {
-            $scope.product = productFinder;
-        },
-        resolve: {
-        	productFinder: function ($stateParams, ProductFactory) {
-        		return ProductFactory.getProduct($stateParams.id);
-        	}
-
-            // // something along these lines to be activated once reviews are implemented
-        	// reviewFinder: function ($stateParams, ProductFactory) {
-        	// 	return ProductFactory.getReviews($stateParams.id);
-        	// }
-        }
+        templateUrl: '/js/products/product-detail/product-detail.html'
     });
 
 });
