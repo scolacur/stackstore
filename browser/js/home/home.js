f07589ca6ea146e130a7d1e83277f46944ddcfdb
Sean Johnston <sean.johnst@gmail.com>
2015-09-16 18:18:57 -0400

    delete comment

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -2,11 +2,5 @@ app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
         templateUrl: 'js/home/home.html'
-        // controller: "ProductListCtrl"
-        // resolve: {
-        // 	findProducts: function (ProductFactory) {
-        // 		return ProductFactory.getProducts();
-        // 	}
-        // }
     });
 });
