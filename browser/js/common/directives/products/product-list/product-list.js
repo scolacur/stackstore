8f9b0bd474bde12fa204ce310d14da374cb8b223
Sean Johnston <sean.johnst@gmail.com>
2015-09-21 09:27:51 -0400

    fix cat nav and add cat edit

--- browser/js/common/directives/products/product-list/product-list.js
+++ browser/js/common/directives/products/product-list/product-list.js
@@ -3,11 +3,18 @@ app.directive("productList", function (ProductFactory) {
     restrict: 'EA',
     templateUrl: 'js/common/directives/products/product-list/product-list.html',
     link: function (scope) {
-      scope.selected = "";
       ProductFactory.getProducts()
       .then(function(products){
         scope.products = products;
       });
+
+      scope.selectedCategory;
+
+      scope.selectedTitle = function () {
+        return JSON.parse('{"title": ""}').title;
+      };
+        
+
       scope.$on('newProduct', function (e, product) {
         scope.products.push(product);
       });
