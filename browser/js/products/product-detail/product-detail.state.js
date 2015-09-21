e48f311ef6450ff1a0aa299e9947d0abdbdb83ee
dwschrashun <dwschrashun@gmail.com>
2015-09-21 14:09:55 -0400

    product details now work with a resolve

--- browser/js/products/product-detail/product-detail.state.js
+++ browser/js/products/product-detail/product-detail.state.js
@@ -3,31 +3,36 @@ app.config(function($stateProvider) {
 	$stateProvider.state('productDetail', {
 		url: '/products/:productId',
 		templateUrl: '/js/products/product-detail/product-detail.html',
-		controller: function($scope, ProductFactory, ReviewFactory, $stateParams, $rootScope, Session) {
-
-			ProductFactory.getProduct($stateParams.productId)
-				.then(function(product) {
-					$scope.product = product;
-
-					if (Session.user){
-						$scope.isAdmin = Session.user.isAdmin;
-						$scope.isOwner = Session.user._id === product.store.user;
-					} else {
-						$scope.isAdmin = false;
-						$scope.isOwner = false;
-					}
-				});
-
+		controller: function($scope, findProduct, findReviews, $stateParams, $rootScope, Session) {
+			$scope.product = findProduct;
+			$scope.reviews = findReviews;
+			if (Session.user){
+				$scope.isAdmin = Session.user.isAdmin;
+				$scope.isOwner = Session.user._id === $scope.product.store.user;
+			} else {
+				$scope.isAdmin = false;
+				$scope.isOwner = false;
+			}
 			$scope.editProduct = function(product) {
 				ProductFactory.editProduct(product._id, product)
 					.then(function() {
 						$rootScope.editMode = false;
 					});
 			};
-			ReviewFactory.getSpecificReviews($stateParams.productId, 'product')
-				.then(function(reviews) {
-					$scope.reviews = reviews;
+
+		},
+		resolve: {
+			findProduct: function (ProductFactory, $stateParams) {
+				return ProductFactory.getProduct($stateParams.productId).then(function (product) {
+					return product;
 				});
+			},
+			findReviews: function (ReviewFactory, $stateParams) {
+				return ReviewFactory.getSpecificReviews($stateParams.productId, 'product')
+				.then(function(reviews) {
+					return reviews;
+				});
+			}
 		}
 	});
 
