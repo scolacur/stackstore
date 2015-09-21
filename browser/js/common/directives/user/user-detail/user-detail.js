cfe4d570114a12251712847a129d0ba58bda35f3
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-09-21 16:49:54 -0400

    fixed merge conflicts

--- browser/js/common/directives/user/user-detail/user-detail.js
+++ browser/js/common/directives/user/user-detail/user-detail.js
@@ -7,23 +7,6 @@ app.directive('userDetail', function(UserFactory, $stateParams, $state, Session)
 		scope.isAdmin = Session.user.isAdmin;
 		scope.editMode = false;
 
-		UserFactory.getById($stateParams.userId)
-		.then(function(user){
-			scope.user = user;
-		});
-		UserFactory.getOrders($stateParams.userId)
-		.then(function (orders) {
-			scope.orders = orders;
-		});
-		UserFactory.getReviews($stateParams.userId)
-		.then(function(reviews){
-			scope.reviews = reviews;
-		});
-		UserFactory.getStores($stateParams.userId)
-		.then(function(stores){
-			scope.stores = stores;
-		});
-
 		scope.enableEdit = function () {
 			scope.cached = angular.copy(scope.user);
 			scope.editMode = true;
