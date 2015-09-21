7ebe359e36dfda46729d08257df5c495dcec1623
dwschrashun <dwschrashun@gmail.com>
2015-09-21 13:19:25 -0400

    moved factory calls to resolves for user detail

--- browser/js/common/directives/user/user-detail/user-detail.js
+++ browser/js/common/directives/user/user-detail/user-detail.js
@@ -3,26 +3,8 @@ app.directive('userDetail', function(UserFactory, $stateParams, $state, $rootSco
 	restrict: 'E',
 	templateUrl: '/js/common/directives/user/user-detail/user-detail.html',
 	link: function (scope){
-				scope.isDetail = true;
-				scope.isAdmin = Session.user.isAdmin;
-
-		UserFactory.getById($stateParams.userId)
-		.then(function(user){
-			scope.user = user;
-		});
-		UserFactory.getOrders($stateParams.userId)
-		.then(function (orders) {
-			scope.orders = orders;
-		});
-		UserFactory.getReviews($stateParams.userId)
-		.then(function(reviews){
-			scope.reviews = reviews;
-		});
-		UserFactory.getStores($stateParams.userId)
-		.then(function(stores){
-			scope.stores = stores;
-		});
-
+		scope.isDetail = true;
+		scope.isAdmin = Session.user.isAdmin;
 		scope.saveUser = function (user) {
 			UserFactory.edit(user._id, user)
 			.then(function (updatedUser) {
@@ -31,6 +13,12 @@ app.directive('userDetail', function(UserFactory, $stateParams, $state, $rootSco
 				console.log('user saved!', updatedUser);
 			});
 		};
+	},
+	scope: {
+		user: "=",
+		stores: "=",
+		reviews: "=",
+		orders: "="
 	}
   };
 });
