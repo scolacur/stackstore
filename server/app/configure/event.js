01178e8aafa43ba03d69b2cc6bab5d2e9cc41ed2
dwschrashun <dwschrashun@gmail.com>
2015-09-22 15:54:35 -0400

    frontend counter now syncs with backend

--- server/app/configure/event.js
+++ server/app/configure/event.js
@@ -1,9 +1,3 @@
 var EventEmitter = require('events').EventEmitter;
 var ee = new EventEmitter();
-module.exports = {ee: ee, newRandom: newRandom};
-
-function newRandom () {
-    setInterval( function () {
-        ee.emit("randomize");
-    }, 10000)
-};
+module.exports = {ee: ee};
