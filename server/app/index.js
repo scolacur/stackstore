424e8dc41a1aa9e6b341e652e1a3b30b5fb70979
Jimin Sung <jimin@Jimins-MacBook-Pro.local>
2015-09-22 12:14:37 -0400

    using event emitters, a randomly chosen product is discounted to free every 10 seconds

--- server/app/index.js
+++ server/app/index.js
@@ -2,6 +2,7 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+require('./configure/event').newRandom();
 module.exports = app;
 
 // Pass our express application pipeline into the configuration
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,11 +1,13 @@
 'use strict';
 var mongoose = require('mongoose');
 var Category = mongoose.model('Category');
+var ee = require('../../app/configure/event.js').ee;
 
 
 var schema = new mongoose.Schema({
     name: {type: String, unique: true},
     price: Number,
+    realPrice: Number,
     description: String,
     inventory: Number,
     photoUrl: {type: String, default: "http://budapestretro.weebly.com/uploads/2/1/6/9/21695204/8297955_orig.jpg?182"},
@@ -37,6 +39,43 @@ schema.statics.createWithDefault = function (reqBody) {
         return self.create(reqBody)
     });
 }
+
+schema.statics.randomDiscount = function () {
+    console.log('random discount called');
+    return mongoose.model('Product').find()
+    .exec()
+    .then (function (products) {
+        return products[Math.floor(Math.random()*products.length)];
+    })
+    .then (function (discountedProduct) {
+        discountedProduct.realPrice = discountedProduct.price;
+        discountedProduct.price = 0;
+        console.log('the new product is: ', discountedProduct);
+        return discountedProduct.save();
+    });
+}
+
+schema.statics.resetOldPrice = function () {
+    console.log('reset old price');
+
+    return mongoose.model('Product').findOne({ realPrice: {$ne: null} })
+    .exec()
+    .then (function (product) {
+        if (!product) return;
+        product.price = product.realPrice;
+        product.realPrice = null;
+        console.log('old discount product found ', product);
+        return product.save();
+    });
+}
+
+ee.on("randomize", function () {
+    console.log('randomize event listened');
+    return mongoose.model('Product').resetOldPrice()
+    .then(function(){
+        mongoose.model('Product').randomDiscount();
+    });
+});
 
 
 mongoose.model('Product', schema);
