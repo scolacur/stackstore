5f32dc13318fd7dde866957ff9cb87fdc347e829
Sean Johnston <sean.johnst@gmail.com>
2015-09-22 11:22:40 -0400

    Update product-list.js

--- browser/js/common/directives/products/product-list/product-list.js
+++ browser/js/common/directives/products/product-list/product-list.js
@@ -1,12 +1,8 @@
-app.directive("productList", function (ProductFactory) {
+app.directive("productList", function () {
   return {
     restrict: 'EA',
     templateUrl: 'js/common/directives/products/product-list/product-list.html',
     link: function (scope) {
-      ProductFactory.getProducts()
-      .then(function(products){
-        scope.products = products;
-      });
 
       scope.selectedTitle = function () {
         return JSON.parse('{"title": ""}').title;
